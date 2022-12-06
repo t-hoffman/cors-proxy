9f0c94d3ad05968dc7f9ffac778defbc545b75f7
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-12-05 21:31:09 -0800

    Fixed extra / in url

--- server.js
+++ server.js
@@ -20,11 +20,12 @@ app.all('*', function (req, res, next) {
         res.send();
     } else {
         var targetURL = req.header('Target-URL');
+        var test = req.header('Test');
         if (!targetURL) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({ url: targetURL, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
